c9859e1dfca92ebfaf773df143e14536d72b26a3
Nhu Bach <nhubach@kms-technology.com>
2016-03-15 16:51:20 +0700

    commit first time to branch1

---
+++ EmployeeApp/EmployeeApp/Controllers/EmployeeApiController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Net;
+
+namespace EmployeeApp.Controllers
+{
+    public class EmployeeApiController : ApiController
+    {
+        static kms_launchEntities data = new kms_launchEntities();
+        List<ASP_API> lst = (from e in data.ASP_API select e).ToList<ASP_API>();
+
+        // GET: EmployeeApi
+        public List<ASP_API> GetAllProducts()
+        {
+            return lst;
+        }
+
+        
+
+    }
+}
---
+++ EmployeeApp/EmployeeApp/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeApp.Models
+{
+    public class Employee
+    {
+        public int ID;
+        public string FirstName;
+        public string MidName;
+        public string LastName;
+        public DateTime DoB;
+    }
+}
